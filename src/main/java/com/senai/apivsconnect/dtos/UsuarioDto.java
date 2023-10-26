77ffdfb9bfef09f72d1238211a2a52d10dcd4cf9
Alan Munoz Martins <alanmunozmartins@gmail.com>
2023-10-25 21:51:06 -0300

    CREATED

---
+++ src/main/java/com/senai/apivsconnect/dtos/UsuarioDto.java
@@ -0,0 +1,18 @@
+package com.senai.apivsconnect.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+
+public record UsuarioDto(
+        @NotBlank String nome,
+        @NotBlank @Email(message = "o email deve estar no formato valodo")String email,
+        @NotBlank String senha,
+        String endereco,
+        String cep,
+        String topo_usuario,
+        String url_img
+) {
+
+}
